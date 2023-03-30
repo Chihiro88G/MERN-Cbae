b2a05f0801a1c18ac7b95d3e116b9098c5e72fcd
gifcolls <108479899+gifcolls@users.noreply.github.com>
2023-03-29 20:33:53 -0400

    URI from mongoDB Atlas

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
+//mongodb+srv://berlioz1188:60h3wLDm11woJbdU@cluster0.kbbt9xs.mongodb.net/?retryWrites=true&w=majority
+//mongodb://localhost/cbae
 
+//module.exports={"URI":"mongodb+srv://berlioz1188:60h3wLDm11woJbdU@cluster0.kbbt9xs.mongodb.net/Cbae?retryWrites=true&w=majoritymongodb+srv://berlioz1188:60h3wLDm11woJbdU@cluster0.kbbt9xs.mongodb.net/?retryWrites=true&w=majority"}
 
 const connectDatabase = () => {
     // need to create "cbae" on local first
